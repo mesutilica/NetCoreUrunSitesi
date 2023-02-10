5d9c8f2b01c71ec876e57f82fe99a55934712c51
mesutilica <mesut.ilica@gmail.com>
2023-02-11 00:25:31 +0300

    11-02-2023 WebAPIUsing eklendi

---
+++ WebAPIUsing/Program.cs
@@ -0,0 +1,61 @@
+using DAL;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Service.Abstract;
+using Service.Concrete;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+builder.Services.AddHttpClient();
+builder.Services.AddDbContext<DatabaseContext>();
+builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddTransient<ICategoryService, CategoryService>();
+builder.Services.AddTransient<IBrandService, BrandService>();
+builder.Services.AddTransient(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(x =>
+{
+    x.LoginPath = "/Admin/Login"; // Admine giriþ yapmayan kullanýcýlarý buraya yönlendir
+    x.AccessDeniedPath = "/AccesDenied";
+    x.LogoutPath = "/Admin/Logout";
+    x.Cookie.Name = "Admin";
+    x.Cookie.MaxAge = TimeSpan.FromDays(7);
+    x.Cookie.IsEssential = true;
+});
+builder.Services.AddAuthorization(x =>
+{
+    x.AddPolicy("AdminPolicy", policy => policy.RequireClaim("Role", "Admin")); // Bundan sonra Controller lara Policy i belirtmeliyiz..
+    x.AddPolicy("UserPolicy", policy => policy.RequireClaim("Role", "User"));
+});
+builder.Services.AddMemoryCache(); // Keþlemeyi aktif etmek için
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+            name: "admin",
+            pattern: "{area:exists}/{controller=Main}/{action=Index}/{id?}"
+          );
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
